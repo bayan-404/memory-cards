62c34cb4e77a3c17f511763f4c15d40af5f62bed
bayan-404 <bayan.jubah@gmail.com>
2022-01-31 20:51:35 +0200

    add a link to the game page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className = 'center'>
-     <p>only 5% of the world's population can match the cards</p>
-     <p>show us how smart you are !</p>
-     <button>Start flipping</button>
+    <div className="center">
+      <p>only 5% of the world's population can match the cards</p>
+      <p>show us how smart you are !</p>
+      <div className="button">
+        <Link href="/game">
+          <a className="start"> Start flipping </a>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
